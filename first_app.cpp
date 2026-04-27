ab2c9fc49a9852efe1244a87aa7e2949d5565c0d
Cudakowski <wojciech.osowski.mjwn@gmail.com>
2026-04-27 09:02:23 +0200

    ep12

--- first_app.cpp
+++ first_app.cpp
@@ -37,23 +37,79 @@ void FirstApp::run() {
   vkDeviceWaitIdle(sspDevice.device());
 }
 
+
+
+
+    // temporary helper function, creates a 1x1x1 cube centered at offset
+    std::unique_ptr<SspModel> createCubeModel(SspDevice& device, glm::vec3 offset) {
+      std::vector<SspModel::Vertex> vertices{
+     
+          // left face (white)
+          {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+          {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+          {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+          {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+          {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+          {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+     
+          // right face (yellow)
+          {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+          {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+          {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+          {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+          {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+          {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+     
+          // top face (orange, remember y axis points down)
+          {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+          {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+          {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+          {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+          {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+          {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+     
+          // bottom face (red)
+          {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+          {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+          {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+          {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+          {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+          {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+     
+          // nose face (blue)
+          {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+          {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+          {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+          {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+          {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+          {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+     
+          // tail face (green)
+          {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+          {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+          {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+          {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+          {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+          {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+     
+      };
+      for (auto& v : vertices) {
+        v.position += offset;
+      }
+      return std::make_unique<SspModel>(device, vertices);
+    }
+
+
+
+
 void FirstApp::loadGameObjects() {
-  std::vector<SspModel::Vertex> vertices{
-      {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-      {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-      {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
-  };
+  std::shared_ptr<SspModel> sspModel = createCubeModel(sspDevice, {0.0f, 0.0f, 0.0f });
 
-  auto sspModel = std::make_shared<SspModel>(sspDevice, vertices);
-
-  auto triangle = SspGameObject::createGameObject();
-  triangle.model = sspModel;
-  triangle.color = {0.1f, 0.8f, 0.1f};
-  triangle.transform2d.translation.x = 0.2f;
-  triangle.transform2d.scale = {2.f,.5f};
-  triangle.transform2d.rotation =  .25f * glm::two_pi<float>();
-
-  gameObjects.push_back(std::move(triangle));
+  auto cube = SspGameObject::createGameObject();
+  cube.model = sspModel;
+  cube.transform.translation = {0.0f, 0.0f, 0.5f};
+  cube.transform.scale = {0.5f, 0.5f, 0.5f};
+  gameObjects.push_back(std::move(cube));
 }
 
 } // namespace ssp
--- ssp_model.hpp
+++ ssp_model.hpp
@@ -17,7 +17,7 @@ class SspModel
 
         struct Vertex
         {
-            glm::vec2 position;
+            glm::vec3 position;
             glm::vec3 color;
 
             static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
