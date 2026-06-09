91813449e6c6ac62b02e7e36c65a4e9eb9283d66
Cudakowski <wojciech.osowski.mjwn@gmail.com>
2026-06-09 19:42:31 +0200

    ep18

--- simple_render_system.cpp
+++ simple_render_system.cpp
@@ -14,7 +14,7 @@ namespace ssp {
 
 struct SimplePushConstantData {
   glm::mat4 transform{1.f};
-  alignas(16) glm::vec3 color;
+  glm::mat4 normalMatrix{1.f};
 };
 
 SimpleRenderSystem::SimpleRenderSystem(SspDevice &device, VkRenderPass renderPass): sspDevice{device} {
@@ -78,8 +78,9 @@ void SimpleRenderSystem::renderGameObjects(
 
   for(auto & obj: gameObjects){
     SimplePushConstantData push{};
-    push.color = obj.color;
-    push.transform = projectionView * obj.transform.mat4();
+    auto modelMatrix = obj.transform.mat4();
+    push.transform = projectionView * modelMatrix;
+    push.normalMatrix = obj.transform.normalMatrix();
 
     vkCmdPushConstants(
       commandBuffer,
